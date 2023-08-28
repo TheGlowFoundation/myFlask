05d8b931d4d756698b0d44b218b1b9e8bc13524a
TheGlowFoundation <132367324+TheGlowFoundation@users.noreply.github.com>
2023-08-28 18:10:37 +0000

    upload of my codes from spaces

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from cs50 import SQL
 
+
+#Configure app
 app = Flask(__name__)
+
+
+#Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 
 @app.route("/")
 def index():
-  return "hello world"
+  if not session.get("name"):
+    return redirect("/login")
+  return render_template("index.html")
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+  if request.method == "POST":
+    session["name"] = request.form.get("name")
+    return redirect("/")
+  return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+  session["name"] = None
+  return redirect("/")
+
+if __name__ == '__main__':
+  app.run(debug=True)
